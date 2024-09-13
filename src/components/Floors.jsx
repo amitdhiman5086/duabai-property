5e14c517692ecc39fd38d493ed1aa0bb76c9453e
Amit Dhiman <amitdhiman5086@gmail.com>
2024-09-14 01:38:27 +0530

    Finally I THink Done

--- src/components/Floors.jsx
+++ src/components/Floors.jsx
@@ -79,7 +79,7 @@ const FloorPlans = () => {
       </p>
 
       {/* Category Tabs */}
-      <div className="flex justify-center mb-8">
+      <div className="flex md:flex-row flex-col gap-2 md:gap-0 justify-center mb-8">
         <button
           className={`px-4 py-2 mx-2 rounded ${
             selectedCategory === "all" ? "bg-green-500 text-white" : "bg-gray-200 text-gray-700"
